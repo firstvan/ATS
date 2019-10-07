f0eab309ce7efdfdbc8b42682c0557a6e0f14bd1
Abdys1 <olalika98@gmail.com>
2019-10-07 17:49:54 +0200

    Project init

---
+++ src/main/java/com/tigra/ats/ApplicationTrackingSystem.java
@@ -0,0 +1,13 @@
+package com.tigra.ats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTrackingSystem {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTrackingSystem.class, args);
+    }
+
+}
