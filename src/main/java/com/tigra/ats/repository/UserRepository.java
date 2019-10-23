e5f6bad3ed695170be34c5770395316f45f26f3b
andrascsoltko <andras.csoltko@gmail.com>
2019-10-23 12:43:23 +0200

    login cuccli

---
+++ src/main/java/com/tigra/ats/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.tigra.ats.repository;
+
+import com.tigra.ats.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    public Optional<User> findByUsername(String username);
+}
