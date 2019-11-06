9e03ae22fb1fcda4bbe1413df43112a7c3c5d2d6
andrascsoltko <andras.csoltko@gmail.com>
2019-11-06 22:13:54 +0100

    Brutforce prev fragmentation

---
+++ src/main/java/com/tigra/ats/service/logic/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,40 @@
+package com.tigra.ats.service.logic;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component("authenticationFailureHandler")
+public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    public String errorMessage;
+
+
+    @Override
+    public void onAuthenticationFailure(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException exception) throws IOException, ServletException {
+        setDefaultFailureUrl("/login?error=true");
+
+        super.onAuthenticationFailure(request, response, exception);
+
+        System.out.println("||||"+exception.getMessage()+"||||");
+
+
+        errorMessage = new String();
+
+        if (exception.getMessage().contains("blocked")) {
+            errorMessage = "A bejelentkezés le lett tíltva, próbálkozzon 5 perc múlva.";
+        }else if(exception.getMessage().contains("Bad credentials")){
+            errorMessage="Rossz felhasználói adatok!";
+        }else if(exception.getMessage().contains("No user found with username: ")){
+            errorMessage="Rossz felhasználói adatok!";
+        }
+
+
+    }
+}
