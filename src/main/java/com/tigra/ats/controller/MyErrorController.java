f05048cf92f348338c27500cb181cb25a6adac56
andrascsoltko <andras.csoltko@gmail.com>
2019-12-16 23:38:38 +0100

    custom white lable error

---
+++ src/main/java/com/tigra/ats/controller/MyErrorController.java
@@ -0,0 +1,20 @@
+package com.tigra.ats.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+class MyErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
